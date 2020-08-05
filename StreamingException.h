870a70bbd830c6603eccf361c628d1bc78011a52
Adrian Cornish <github@bluedreamer.com>
2020-08-05 10:52:19 -0600

    Added locations to exceptions

--- StreamingException.h
+++ StreamingException.h
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <memory>
+#include <optional>
 #include <sstream>
 #include <stdexcept>
 
@@ -12,6 +13,27 @@ public:
       : std::runtime_error("")
       , m_stream(std::make_shared<std::stringstream>())
       , m_message()
+      , m_filename()
+      , m_function()
+      , m_line_number()
+   {
+   }
+   StreamingException(std::string filename, std::string function, int line_number)
+      : std::runtime_error("")
+      , m_stream(std::make_shared<std::stringstream>())
+      , m_message()
+      , m_filename(std::move(filename))
+      , m_function(std::move(function))
+      , m_line_number(line_number)
+   {
+   }
+   StreamingException(std::string function, int line_number)
+      : std::runtime_error("")
+      , m_stream(std::make_shared<std::stringstream>())
+      , m_message()
+      , m_filename()
+      , m_function(std::move(function))
+      , m_line_number(line_number)
    {
    }
    StreamingException(const StreamingException &) = default;
@@ -29,13 +51,34 @@ public:
 
    auto what() const noexcept -> const char * override
    {
-      m_message = m_stream->str();
+      std::ostringstream location;
+      if(m_filename && m_function && m_line_number)
+      {
+         location << m_filename.value() << ':' << m_function.value() << ':' << m_line_number.value() << ' ';
+      }
+      else if(m_function && m_line_number)
+      {
+         location << m_function.value() << ':' << m_line_number.value() << ' ';
+      }
+      auto loc_string = location.str();
+      if(!loc_string.empty())
+      {
+         m_message = loc_string;
+      }
+      m_message += m_stream->str();
       return m_message.c_str();
    }
+
+   auto filename() const noexcept -> std::string { return m_filename.value_or("not set"); }
+   auto function() const noexcept -> std::string { return m_function.value_or("not set"); }
+   auto line_number() const noexcept -> int { return m_line_number.value_or(-1); }
 
 private:
    mutable std::shared_ptr<std::stringstream> m_stream;
    mutable std::string                        m_message;
+   std::optional<std::string>                 m_filename;
+   std::optional<std::string>                 m_function;
+   std::optional<int>                         m_line_number;
 };
 
 // vim: ts=3 sw=3 ai et nohls mps=(\:),{\:},[\:],<\:> ff=unix ffs=unix bg=dark
--- main.cpp
+++ main.cpp
@@ -3,11 +3,27 @@
 #include "StreamingException.h"
 
 auto function() -> void;
+auto function2() -> void;
+auto function3() -> void;
+auto function4() -> void;
+
+auto test(void (*function)()) -> void;
 
 auto main(int argc, char *argv[]) -> int
 {
    (void)argc;
    (void)argv;
+
+   test(function);
+   test(function2);
+   test(function3);
+   test(function4);
+
+   return 0;
+}
+
+auto test(void (*function)()) -> void
+{
    try
    {
       function();
@@ -16,8 +32,6 @@ auto main(int argc, char *argv[]) -> int
    {
       std::cout << "Exception: " << e.what() << std::endl;
    }
-
-   return 0;
 }
 
 auto function() -> void
@@ -27,6 +41,33 @@ auto function() -> void
    std::string s{"I kanna do it Captain!"};
 
    throw StreamingException() << "This is my error: " << i << ", " << d << " because: " << s;
+}
+
+auto function2() -> void
+{
+   int         i{42};
+   double      d{3.142};
+   std::string s{"I kanna do it Captain!"};
+
+   throw StreamingException(__FILE__, __func__, __LINE__) << "This is my error: " << i << ", " << d << " because: " << s;
+}
+
+auto function3() -> void
+{
+   int         i{42};
+   double      d{3.142};
+   std::string s{"I kanna do it Captain!"};
+
+   throw StreamingException(__FILE__, __PRETTY_FUNCTION__, __LINE__) << "This is my error: " << i << ", " << d << " because: " << s;
+}
+
+auto function4() -> void
+{
+   int         i{42};
+   double      d{3.142};
+   std::string s{"I kanna do it Captain!"};
+
+   throw StreamingException(__PRETTY_FUNCTION__, __LINE__) << "This is my error: " << i << ", " << d << " because: " << s;
 }
 
 // vim: ts=3 sw=3 ai et nohls mps=(\:),{\:},[\:],<\:> ff=unix ffs=unix bg=dark
